521ba9c91010d0d52c3f5ec182f7258e4562e17b
tangj <1844250138@qq.com>
2021-07-30 14:55:01 +0800

    Add embedding_ops

--- easy_rec_ext/test/dynamic_embedding_test.py
+++ easy_rec_ext/test/dynamic_embedding_test.py
@@ -3,14 +3,46 @@
 # time: 2021/7/26 3:40 下午
 # desc:
 
+from tensorflow.python.framework import ops
+
 import tensorflow as tf
 
-if tf.__version__ >= '2.0':
+if tf.__version__ >= "2.0":
     tf = tf.compat.v1
 
 import tensorflow_recommenders_addons as tfra
 
 line_sep = "\n" + "##" * 20 + "\n"
+
+
+def test_03():
+    tf.disable_eager_execution()
+
+    w1 = tfra.dynamic_embedding.get_variable(
+        name="w1",
+        key_dtype=tf.dtypes.int64,
+        dim=4,
+        initializer=tf.random_normal_initializer(0, 0.1),
+    )
+
+    ids2 = tf.SparseTensor(
+        indices=[(0, 0), (1, 0), (2, 0), (3, 0)],
+        values=tf.constant(value=[1, 1, -1, -1], dtype=tf.dtypes.int64),
+        dense_shape=[5, 1]
+    )
+    v2 = tfra.dynamic_embedding.safe_embedding_lookup_sparse(
+        embedding_weights=w1,
+        sparse_ids=ids2
+    )
+
+    with tf.Session() as sess:
+        print(line_sep)
+        print(sess.run(tf.sparse_tensor_to_dense(ids2)))
+        print(line_sep)
+        print(sess.run(v2))
+
+        print(line_sep)
+        print(sess.run(ops.convert_to_tensor(w1)))
 
 
 def test_02():
@@ -21,41 +53,35 @@ def test_02():
         key_dtype=tf.dtypes.string,
         dim=4,
         initializer=tf.random_normal_initializer(0, 0.1),
-        init_size=16,
+        init_size=1,
     )
-    # print(line_sep)
-    # print(w1)
 
     v1 = tfra.dynamic_embedding.embedding_lookup(
         params=w1,
         ids=tf.constant(value=[["431645d82843f859"], ["42fe503ee7820de1ce6420747820813b"]], dtype=tf.dtypes.string)
     )
-    # print(line_sep)
-    # print(v1)
 
     v2 = tfra.dynamic_embedding.embedding_lookup(
         params=w1,
         ids=tf.constant(value=[["431645d82843f859"], ["431645d82843f859"]], dtype=tf.dtypes.string)
     )
-    # print(line_sep)
-    # print(v2)
 
     v3 = tfra.dynamic_embedding.embedding_lookup(
         params=w1,
         ids=tf.constant(value=[[""], [""]], dtype=tf.dtypes.string)
     )
-    # print(line_sep)
-    # print(v3)
 
     with tf.Session() as sess:
-        print(line_sep)
-        print(sess.run(w1))
+        # print(line_sep)
+        # print(sess.run(w1))
         print(line_sep)
         print(sess.run(v1))
         print(line_sep)
         print(sess.run(v2))
         print(line_sep)
         print(sess.run(v3))
+        print(line_sep)
+        print(sess.run(tf.convert_to_tensor(w1)))
 
 
 def test_01():
@@ -64,7 +90,7 @@ def test_01():
     w1 = tfra.dynamic_embedding.get_variable(
         name="w1",
         dim=4,
-        initializer=tf.random_normal_initializer(0, 0.1),
+        initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1),
     )
     print(line_sep)
     print(w1)
