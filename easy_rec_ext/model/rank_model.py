54f5b9f5f0aea3376c2a6dfa6da8b9158ffd7423
tangj <1844250138@qq.com>
2021-08-16 19:18:36 +0800

    Update

--- easy_rec_ext/model/rank_model.py
+++ easy_rec_ext/model/rank_model.py
@@ -104,15 +104,17 @@ class RankModel(object):
                 ids = self._feature_dict[feature_field.input_name]
                 if ids.dtype == tf.dtypes.string:
                     embedding_weights = embedding_ops.get_embedding_variable(
-                        feature_field.embedding_name,
-                        feature_field.embedding_dim,
-                        True
+                        name=feature_field.embedding_name,
+                        dim=feature_field.embedding_dim,
+                        vocab_size=feature_field.num_buckets if feature_field.num_buckets>0 else feature_field.hash_bucket_size,
+                        key_is_string=True,
                     )
                 else:
                     embedding_weights = embedding_ops.get_embedding_variable(
-                        feature_field.embedding_name,
-                        feature_field.embedding_dim,
-                        False
+                        name=feature_field.embedding_name,
+                        dim=feature_field.embedding_dim,
+                        vocab_size=feature_field.num_buckets if feature_field.num_buckets > 0 else feature_field.hash_bucket_size,
+                        key_is_string=False,
                     )
                 values = embedding_ops.safe_embedding_lookup(
                     embedding_weights, ids
@@ -121,8 +123,10 @@ class RankModel(object):
                 values = self._feature_dict[feature_field.input_name]
             elif feature_field.feature_type == "SequenceFeature":
                 embedding_weights = embedding_ops.get_embedding_variable(
-                    feature_field.embedding_name,
-                    feature_field.embedding_dim
+                    name=feature_field.embedding_name,
+                    dim=feature_field.embedding_dim,
+                    vocab_size=feature_field.num_buckets if feature_field.num_buckets > 0 else feature_field.hash_bucket_size,
+                    key_is_string=False,
                 )
                 values = embedding_ops.safe_embedding_lookup(
                     embedding_weights, self._feature_dict[feature_field.input_name],
@@ -145,7 +149,9 @@ class RankModel(object):
 
         key_embedding_weights = embedding_ops.get_embedding_variable(
             key_feature_field.embedding_name,
-            key_feature_field.embedding_dim
+            key_feature_field.embedding_dim,
+            vocab_size=key_feature_field.num_buckets if key_feature_field.num_buckets > 0 else key_feature_field.hash_bucket_size,
+            key_is_string=False,
         )
         outputs["key"] = embedding_ops.safe_embedding_lookup(
             key_embedding_weights, self._feature_dict[key_feature_field.input_name],
@@ -157,7 +163,9 @@ class RankModel(object):
         hist_seq = self._feature_dict[seq_feature_field.input_name]
         seq_embedding_weights = embedding_ops.get_embedding_variable(
             seq_feature_field.embedding_name,
-            seq_feature_field.embedding_dim
+            seq_feature_field.embedding_dim,
+            vocab_size=seq_feature_field.num_buckets if seq_feature_field.num_buckets > 0 else seq_feature_field.hash_bucket_size,
+            key_is_string=False,
         )
         hist_seq_emb = embedding_ops.safe_embedding_lookup(
             seq_embedding_weights, tf.expand_dims(hist_seq, -1)
