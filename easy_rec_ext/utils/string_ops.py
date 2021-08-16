c8cbb7b5bcbb49a4377262eebb6a2fd0dc147ed4
tangj <1844250138@qq.com>
2021-08-16 16:45:55 +0800

    Update

--- easy_rec_ext/utils/string_ops.py
+++ easy_rec_ext/utils/string_ops.py
@@ -11,7 +11,8 @@ if tf.__version__ >= "2.0":
 
 def string_to_hash_bucket(input_tensor, num_buckets):
     condition = tf.equal(input_tensor, "")
-    t1 = tf.string_to_hash_bucket_strong(input_tensor, num_buckets, [555, 1234])
+    # t1 = tf.string_to_hash_bucket_strong(input_tensor, num_buckets, [555, 1234])
+    t1 = tf.string_to_hash_bucket_fast(input_tensor, num_buckets)
     t2 = tf.zeros_like(t1) - 1
     return tf.where(
         condition,
