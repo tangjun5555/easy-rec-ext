a8431d321457e1cd402f62537f7daf1410f3be85
tangj <1844250138@qq.com>
2021-09-09 19:12:38 +0800

    Update

--- easy_rec_ext/core/activation.py
+++ easy_rec_ext/core/activation.py
@@ -9,35 +9,34 @@ if tf.__version__ >= "2.0":
     tf = tf.compat.v1
 
 
-def parametric_relu(name, _x):
+def parametric_relu(name, inputs):
     with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
         alphas = tf.get_variable(name="alpha",
-                                 shape=_x.get_shape()[-1],
+                                 shape=inputs.get_shape()[-1],
                                  initializer=tf.constant_initializer(0.0),
                                  dtype=tf.float32,
                                  )
-    pos = tf.nn.relu(_x)
-    neg = alphas * (_x - tf.math.abs(_x)) * 0.5
+    pos = tf.nn.relu(inputs)
+    neg = alphas * (inputs - tf.math.abs(inputs)) * 0.5
     return pos + neg
 
 
-def dice(name, _x, axis=-1, epsilon=1e-9, ):
+def dice(name, inputs, is_training, axis=-1, epsilon=1e-9):
     with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
         alphas = tf.get_variable(name="alpha",
-                                 shape=_x.get_shape()[-1],
+                                 shape=inputs.get_shape()[-1],
                                  initializer=tf.constant_initializer(0.0),
                                  dtype=tf.float32,
                                  )
-        input_shape = list(_x.get_shape())
-        reduction_axes = list(range(len(input_shape)))
-        del reduction_axes[axis]
-        broadcast_shape = [1] * len(input_shape)
-        broadcast_shape[axis] = input_shape[axis]
-    mean = tf.reduce_mean(_x, axis=reduction_axes)
-    brodcast_mean = tf.reshape(mean, broadcast_shape)
-    std = tf.reduce_mean(tf.square(_x - brodcast_mean) + epsilon, axis=reduction_axes)
-    std = tf.sqrt(std)
-    brodcast_std = tf.reshape(std, broadcast_shape)
-    x_normed = (_x - brodcast_mean) / (brodcast_std + epsilon)
-    x_p = tf.sigmoid(x_normed)
-    return alphas * (1.0 - x_p) * _x + x_p * _x
+    inputs_normed = tf.layers.batch_normalization(
+        inputs,
+        axis=axis,
+        epsilon=epsilon,
+        name="%s/bn" % name,
+        center=False,
+        scale=False,
+        training=is_training,
+        trainable=True,
+    )
+    x_p = tf.sigmoid(inputs_normed)
+    return alphas * (1.0 - x_p) * inputs + x_p * inputs
