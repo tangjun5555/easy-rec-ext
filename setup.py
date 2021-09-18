aa8f1bb81b6275b3170de3c394935ce4d098c4ac
tangj <1844250138@qq.com>
2021-09-18 16:23:40 +0800

    Upgrade version to 0.0.5

--- setup.py
+++ setup.py
@@ -6,25 +6,25 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-version = "0.0.4"
+version = "0.0.5"
 
 setup(
-  name="easy_rec_ext",
-  version=version,
-  description="An TensorFlow Framework For Recommender System",
-  packages=find_packages(),
-  author="tangj",
-  author_email="1844250138@qq.com",
-  python_requires=">=3.5",
-  install_requires=Path("requirements.txt").read_text().splitlines(),
-  extras_require={
-    "cpu": [
-      "tensorflow>=2.4.1",
-      "tensorflow-recommenders-addons>=0.2.0"
-    ],
-    "gpu": [
-      "tensorflow-gpu>=2.4.1",
-      "tensorflow-recommenders-addons-gpu>=0.2.0"
-    ],
-  },
+    name="easy_rec_ext",
+    version=version,
+    description="An TensorFlow Framework For Recommender System",
+    packages=find_packages(),
+    author="tangj",
+    author_email="1844250138@qq.com",
+    python_requires=">=3.5",
+    install_requires=Path("requirements.txt").read_text().splitlines(),
+    extras_require={
+        "cpu": [
+            "tensorflow>=2.4.1",
+            "tensorflow-recommenders-addons>=0.2.0"
+        ],
+        "gpu": [
+            "tensorflow-gpu>=2.4.1",
+            "tensorflow-recommenders-addons-gpu>=0.2.0"
+        ],
+    },
 )
