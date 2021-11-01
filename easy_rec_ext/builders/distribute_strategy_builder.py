7be946f538a701881b5fb1556ce040057246c8cb
tangj <1844250138@qq.com>
2021-11-01 18:43:17 +0800

    update

--- easy_rec_ext/builders/distribute_strategy_builder.py
+++ easy_rec_ext/builders/distribute_strategy_builder.py
@@ -10,21 +10,13 @@ from easy_rec_ext.core.pipeline import TrainConfig
 def build(train_config):
     """
     Create distribute training strategy based on config.
-    Args:
-        train_config:
-
-    Returns:
-
     """
     assert isinstance(train_config, TrainConfig)
     # single worker multi-gpu strategy
     if train_config.train_distribute == "MirroredStrategy":
         distribution = tf.distribute.MirroredStrategy()
-    # multi worker multi-gpu strategy
-    # works under tf1.15 and tf2.x
-    elif train_config.train_distribute == "MultiWorkerMirroredStrategy":
-        distribution = tf.distribute.MultiWorkerMirroredStrategy()
-    # works under tf1.15 and tf2.x
+    # multi-worker strategy with parameter servers
+    # under tf1.15 and tf2.x
     elif train_config.train_distribute == "PSStrategy":
         distribution = tf.distribute.experimental.ParameterServerStrategy()
     else:
