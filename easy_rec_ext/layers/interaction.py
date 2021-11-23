7c3c4ec0566af412b6c395895d03a73ad94dd92c
tangj <1844250138@qq.com>
2021-11-23 10:47:29 +0800

    interaction add CAN

--- easy_rec_ext/layers/interaction.py
+++ easy_rec_ext/layers/interaction.py
@@ -7,6 +7,7 @@ import os
 import logging
 
 import tensorflow as tf
+from easy_rec_ext.core import embedding_ops
 
 tf = tf.compat.v1
 
@@ -84,7 +85,7 @@ class InnerProduct(object):
 
 
 class CAN(object):
-    def __init__(self, name, mode, dimension, order, mlp_units=(64, 32)):
+    def __init__(self, name, mode, item_vocab_size, order=2, mlp_units=(8, 4)):
         """
         Co-action Network
         Args:
@@ -93,24 +94,73 @@ class CAN(object):
         """
         self.name = name
         self.mode = mode
-        self.dimension = dimension
+        self.item_vocab_size = item_vocab_size
         self.order = order
         self.mlp_units = mlp_units
 
     def __call__(self, user_value, item_value):
         """
-        TODO
         Args:
             user_value:
                 3D tensor with shape: (batch_size, user_field_num, embedding_size)
                 fed into MLP
             item_value:
-                3D tensor with shape: (batch_size, item_field_num, embedding_size)
+                2D tensor with shape: (batch_size, 1), use the original id
                 server as the weight and bias
         Returns:
-
+            - 2D tensor with shape: (batch_size, order * mlp_units[-1])
         """
-        user_value_shape = user_value.get_shape().as_list()
-        item_value_shape = item_value.get_shape().as_list()
+        user_dimension = user_value.get_shape().as_list()[-1]
+        item_dimension = 0
+        weight_emb_w = []
+        weight_emb_b = []
+        x = user_dimension
+        for y in self.mlp_units:
+            item_dimension += x * y
+            item_dimension += 1
 
-        assert item_value_shape[-1] == sum(self.mlp_units) + len(self.mlp_units)
+            weight_emb_w.append([x, y])
+            weight_emb_b.append(1)
+
+            x = y
+
+        item_embedding_weight = embedding_ops.get_embedding_variable(
+            name=self.name + "_item",
+            dim=item_dimension,
+            vocab_size=self.item_vocab_size
+        )
+        item_embedding_value = embedding_ops.safe_embedding_lookup(
+            item_embedding_weight, item_value,
+        )
+
+        weight, bias = [], []
+        idx = 0
+        weight_orders = []
+        bias_orders = []
+        for i in range(self.order):
+            for w, b in zip(weight_emb_w, weight_emb_b):
+                weight.append(tf.slice(item_embedding_value, [0, idx], [-1, w[0] * w[1]]))
+                idx += w[0] * w[1]
+                bias.append(tf.slice(item_embedding_value, [0, idx], [-1, b]))
+                idx += b
+            weight_orders.append(weight)
+            bias_orders.append(bias)
+
+        out_seq = []
+        hh = [user_value]
+        for i in range(self.order - 1):
+            hh.append(tf.multiply(hh[-1] ** user_value))
+        for i, h in enumerate(hh):
+            weight, bias = weight_orders[i], bias_orders[i]
+            for j, (w, b) in enumerate(zip(weight, bias)):
+                h = tf.matmul(h, w)
+                h = h + b
+                if j != len(weight) - 1:
+                    h = tf.nn.tanh(h)
+            out_seq.append(h)
+        out_seq = tf.concat(out_seq, 2)
+
+        # if mask is not None:
+        #     mask = tf.expand_dims(mask, axis=-1)
+        #     out_seq = out_seq * mask
+        return tf.reduce_sum(out_seq, 1, keepdims=False)
