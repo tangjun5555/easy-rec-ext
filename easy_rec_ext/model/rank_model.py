e6c0d85abdb6a1b4024625dcaf8da0d6d88a3b29
tangj <1844250138@qq.com>
2021-11-30 15:38:55 +0800

    update

--- easy_rec_ext/model/rank_model.py
+++ easy_rec_ext/model/rank_model.py
@@ -153,14 +153,16 @@ class RankModel(object):
 
         for i in range(feature_fields_num):
             feature_field = self._feature_fields_dict[feature_group.feature_names[i]]
-            if feature_field.feature_type == "IdFeature":
-                outputs.append(group_input_dict[feature_field.input_name])
-            elif feature_field.feature_type == "RawFeature":
+            # if feature_field.feature_type == "IdFeature":
+            #     outputs.append(group_input_dict[feature_field.input_name])
+            if feature_field.feature_type == "RawFeature":
                 outputs.append(
                     tf.reshape(group_input_dict[feature_field.input_name], [-1, feature_field.raw_input_dim, feature_field.embedding_dim])
                 )
             else:
-                raise ValueError("build_interaction_input_layer, feature_type: %s not supported." % feature_field.feature_type)
+                outputs.append(group_input_dict[feature_field.input_name])
+            # else:
+            #     raise ValueError("build_interaction_input_layer, feature_type: %s not supported." % feature_field.feature_type)
 
         outputs = tf.stack(outputs, axis=1)
         return outputs
