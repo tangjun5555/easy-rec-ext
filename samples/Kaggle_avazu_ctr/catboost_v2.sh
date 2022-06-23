1a2c8cd2e794ef151c6fdc83ae6bcd4ef84d45ea
tangj <1844250138@qq.com>
2022-06-23 14:26:36 +0800

    Update Sample Kaggle_avazu_ctr.

--- samples/Kaggle_avazu_ctr/catboost_v2.sh
+++ samples/Kaggle_avazu_ctr/catboost_v2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# logloss:0.412462986	auc:0.7163743267
-rm -rf catboost_v1
+# logloss:0.4080213942	auc:0.7262751619
+rm -rf catboost_v2
 catboost fit \
   --used-ram-limit=8gb \
   --thread-count=4 \
   --logging-level=Debug \
   --metric-period=1 \
-  --train-dir=catboost_v1 \
+  --train-dir=catboost_v2 \
   --model-format=json \
   --learn-set=train_27_29.csv \
   --test-set=train_30.csv \
   --delimiter=',' \
-  --column-description=feature_v1.cd  \
+  --column-description=feature_v2.cd  \
   --loss-function=Logloss \
   --custom-metric="Logloss:hints=skip_train~false,AUC:hints=skip_train~false" \
   --random-seed=555 \
