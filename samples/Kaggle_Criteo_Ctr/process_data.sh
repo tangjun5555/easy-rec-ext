8bd0113b514625fd70147c45c9ff2d20979eec88
tangj <1844250138@qq.com>
2022-10-18 19:27:59 +0800

    Add Sample Kaggle_Criteo_Ctr.

---
+++ samples/Kaggle_Criteo_Ctr/process_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z ${easy_rec_ext_data_dir} ]; then
+  echo "easy_rec_ext_data_dir is not exists"
+  exit 1
+else
+  echo "easy_rec_ext_data_dir=${easy_rec_ext_data_dir}"
+fi
+
+cd ${easy_rec_ext_data_dir}
+split -6000000 -d train.txt sample_
